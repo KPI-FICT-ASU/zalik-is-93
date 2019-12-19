a96f0a69580938668a7fe063b505d82938cc3c1e
Malanchyk <56121409+Malanchyk@users.noreply.github.com>
2019-12-19 11:36:30 +0200

    Add files via upload

---
+++ RE_SULT.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+touch test1.txt
+touch test2.txt
+open test1.txt
+open test2.txt
+cp /Users/illiamalanchuk3228/test1.txt /Users/illiamalanchuk3228/NEW_FOLDER
+cp /Users/illiamalanchuk3228/tets2.txt /Users/illiamalanchuk3228/NEW_FOLDER
+nl NEW_FOLDER
+
